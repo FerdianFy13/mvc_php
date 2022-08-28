e26fcc2d66a7a7006fa605f8ff097f7aa99d091b
FerdianFy13 <ferdianfy13@gmail.com>
2022-08-28 15:01:27 +0700

    update ui mahasiswa

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,12 +1,18 @@
 <div class="container mt-5">
+    <div class="row">
     <div class="col-6">
         <h3>List Students</h3>
+        <ul class="list-group">
         <?php foreach ($data['mahasiswa'] as $mhs): ?>
-        <ul>
-            <li><?= $mhs['name'] ?></li>
-            <li><?= $mhs['nim'] ?></li>
-            <li><?= $mhs['major'] ?></li>
-        </ul>
+            <li class="list-group-item d-flex justify-content-between align-items-start"><?= $mhs[
+                'name'
+            ] ?>
+                <a href="<?= BASEURL ?>/mahasiswa/detail/<?= $mhs[
+    'id_mhs'
+] ?>" class="badge bg-primary text-decoration-none ">Details</a>
+            </li>
         <?php endforeach; ?>
+        </ul>
+    </div>
     </div>
 </div>
